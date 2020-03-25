96d615e3c8d2b9e25afeee5f996579d62bb78357
tony-yuan33 <ruihong.19@intl.zju.edu.cn>
2020-03-26 01:00:16 +0800

    Comments on mechanisms of "mystery_code.py".

--- Practical5/mystery_code.py
+++ Practical5/mystery_code.py
@@ -1,5 +1,5 @@
 # What does this piece of code do?
-# Answer: 
+# Answer: Finds a random prime in [1..100].
 
 # Import libraries
 # randint allows drawing a random number, 
@@ -11,14 +11,16 @@ from random import randint
 from math import ceil
 
 p=False
-while p==False:
+while p==False: # Exits when p == True.
     p=True
     n = randint(1,100)
     u = ceil(n**(0.5))
     for i in range(2,u+1):
         if n%i == 0:
-            p=False
+            p=False # There exists i in [2..`sqrt`(n)] such that i divides n
 
-
+# Finds an n in [1..100] such that it does not have a factor in [2..ceil(sqrt(n))].
+# In other words, n is a prime, because if n = ab with a, b > 2, then either a < sqrt(n)
+# or b < sqrt(n). Either way, one factor lies in the range [2..sqrt(n)].
      
 print(n)
