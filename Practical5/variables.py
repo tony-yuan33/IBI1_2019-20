91b7f81ef15fec92865fee28487556a9d64b632b
tony-yuan33 <ruihong.19@intl.zju.edu.cn>
2020-03-26 00:32:17 +0800

    Added boolean section

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -4,3 +4,9 @@ c = b / 7
 d = c / 11
 e = d / 13
 print(e < a, e > a)
+
+def z(x, y): return (x and not y) or (y and not x)
+def w(x, y): return x != y
+
+# By reflexivity, only one heterogeneous pair is tested.
+print(z(True, True)==w(True, True), z(False, False)==w(False, False), z(True, False)==w(True, False))
