8bc977a041422b59fd017c74c0691410e94b127a
tony-yuan33 <ruihong.19@intl.zju.edu.cn>
2020-03-26 00:40:43 +0800

    Added explanation of numbers section and comments

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -1,12 +1,16 @@
 a = 999
 b = 999999
+print(b % 7 == 0) # True
 c = b / 7
 d = c / 11
 e = d / 13
-print(e < a, e > a)
+print(e < a, e > a) # False, False
+
+# For any three-digit positive integer n=(abc)[10], the number m=(abcabc)[10]
+# is 1001*n. It is because (abcabc) = (abc000) + (abc) = (abc)*1000 + (abc).
 
 def z(x, y): return (x and not y) or (y and not x)
 def w(x, y): return x != y
 
 # By reflexivity, only one heterogeneous pair is tested.
-print(z(True, True)==w(True, True), z(False, False)==w(False, False), z(True, False)==w(True, False))
+print(z(True, True)==w(True, True), z(False, False)==w(False, False), z(True, False)==w(True, False)) # True, True, True
