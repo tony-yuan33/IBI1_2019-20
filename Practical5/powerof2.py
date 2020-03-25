99ddb850a480d6009c26a1cfb55543e1d24e6b24
tony-yuan33 <ruihong.19@intl.zju.edu.cn>
2020-03-26 01:14:09 +0800

    Comment "powerof2.py"

--- Practical5/powerof2.py
+++ Practical5/powerof2.py
@@ -1,13 +1,14 @@
 x = 2019
 k = 0
-li = []
+li = [] # Contains all terms to be displayed.
 
 while x > 0:
-    while not(x & 1):
+    while not(x & 1): # Remove trailing zeros.
         x >>= 1
         k += 1
-    li.append('2**' + str(k))
+    li.append('2**' + str(k)) # Append new term
     x >>= 1
     k += 1
+
 li.reverse()
 print(' + '.join(li))
