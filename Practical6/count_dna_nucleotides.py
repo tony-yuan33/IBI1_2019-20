60c85266c1fa2457e95e02ae89f91283c792b33f
tony-yuan33 <ruihong.19@intl.zju.edu.cn>
2020-05-08 22:22:40 +0800

    Implement "count_dna_nucleotides.py"

--- Practical6/count_dna_nucleotides.py
+++ Practical6/count_dna_nucleotides.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plot
 
 dna = "ATGCTTCAGAAAGGTCTTACG"
+
+freq_tab = {'A':0, 'T':0, 'C':0, 'G':0}
+for base in dna:
+    freq_tab[base] += 1
+
+plot.pie(freq_tab.values(), labels=freq_tab.keys(), autopct="%1.1f%%")
