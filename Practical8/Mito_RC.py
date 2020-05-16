56281cbec5d27e7a5d8c3207b5d8f00ddb7a0f6d
tony-yuan33 <ruihong.19@intl.zju.edu.cn>
2020-05-17 05:42:22 +0800

    Comments added

--- Practical8/Mito_RC.py
+++ Practical8/Mito_RC.py
@@ -9,18 +9,18 @@ with open("mito_gene.fa", 'r') as mito_file, open(filename, 'w') as out_file:
     lines = mito_file.readlines()
     i, n = 0, len(lines)
     while i < n:
-        res = re.match(re_name, lines[i].rstrip())
-        out_file.write(res.group(1) + ' ')
+        res = re.match(re_name, lines[i].rstrip()) # match gene name
+        out_file.write(res.group(1) + ' ') # write gene name
         i += 1
 
         seq = ''
-        while i < n and not lines[i].startswith('>'):
-            seq += lines[i].rstrip()
-            i += 1
+        while i < n and not lines[i].startswith('>'): # for each following line that is still sequence
+            seq += lines[i].rstrip() # append to whole sequence
+            i += 1                   # go to next line
         
-        out_file.write(str(len(seq)))
+        out_file.write(str(len(seq))) # write sequence length
         out_file.write('\n')
-        out_file.write(seq[::-1].translate(trans))
+        out_file.write(seq[::-1].translate(trans)) # write RC sequence
         out_file.write('\n')
     
     out_file.flush()
